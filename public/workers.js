35a3835f42fa1c0500e28bfb2038a7cef396a423
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-04-30 14:24:16 -0500

    css gradient added to background

---
+++ public/workers.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-restricted-globals */
+//name of your app
+const CACHE_NAME = 'memory-game';
+//include all routes used in app
+const urlsToCache = ['.'];
+
+// Install a service worker
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', (event) => {
+  var cacheWhitelist = ['memory-game'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
